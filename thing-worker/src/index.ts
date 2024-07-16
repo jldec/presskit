32863d10a5cec7fe9a4677127f7a8a5dee3eab38
jldec <jurgen@jldec.me>
2024-07-16 14:15:28 +0100

    route content, 404, debug config

--- thing-worker/src/index.ts
+++ thing-worker/src/index.ts
@@ -1,12 +1,22 @@
 import { Hono } from 'hono'
+import { StatusCode } from 'hono/utils/http-status'
 
 const app = new Hono()
 
-const fileUrl = 'https://raw.githubusercontent.com/jldec/thing/main/content/new-thing.md'
+const fileUrlPrefix = 'https://raw.githubusercontent.com/jldec/thing/main/content'
+const indexFile = 'index.md'
 
 app.get('/', async (c) => {
-	const req = await fetch(fileUrl)
-	return c.text(await req.text())
+	const response = await fetch(`${fileUrlPrefix}/${indexFile}`)
+	return c.text(await response.text())
+})
+
+// Translate request path into file URL, including .md extension
+// https://hono.dev/docs/api/routing#including-slashes
+app.get('/:path{.+$}', async (c) => {
+	const { path } = c.req.param()
+	const response = await fetch(`${fileUrlPrefix}/${path}.md`)
+	return c.text(await response.text(), response.status as StatusCode)
 })
 
 export default app
