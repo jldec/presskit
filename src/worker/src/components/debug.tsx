b6cb9046ab4d28742b04efbb2c4881dd0978c1fc
jldec <jurgen@jldec.me>
2024-10-12 21:14:00 +0100

    show user in debug, show debug in prod

--- src/worker/src/components/debug.tsx
+++ src/worker/src/components/debug.tsx
@@ -3,10 +3,12 @@ import { useRequestContext } from 'hono/jsx-renderer'
 
 export const Debug: FC = ({ page }) => {
   const c = useRequestContext()
-  if (c.env.ENVIRONMENT === 'dev' && page)
+  const user = c.req.header('cf-access-authenticated-user-email')
+  if (page /* && c.env.ENVIRONMENT === 'dev'*/)
     return (
       <details class="mt-4 cursor-pointer">
         <summary class="text-black/30 dark:text-white/40">debug</summary>
+        <h4>user: {user}</h4>
         <pre>{JSON.stringify(page, null, 2)}</pre>
       </details>
     )
