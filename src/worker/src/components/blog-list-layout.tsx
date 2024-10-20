3c18731c574571a11e1dc1e853b99e3b746d624d
Jürgen Leschner <jldec@users.noreply.github.com>
2024-10-20 10:24:55 +0100

    dates, deps (#18)

--- src/worker/src/components/blog-list-layout.tsx
+++ src/worker/src/components/blog-list-layout.tsx
@@ -9,9 +9,18 @@ export const BlogListLayout: FC = ({ children, page }) => {
       <ul>
         {page?.dir?.map((dirPage: DirData) => {
           const text = dirPage.attrs?.title ?? dirPage.path
+          let date = dirPage.attrs?.date ?? ''
           return (
             <li>
-              <a class="navlink" href={dirPage.path}>{text}</a>
+              <a
+                class="flex gap-4 text-transparent hover:text-gray-300 no-underline border-b-[1.5px] border-b-transparent hover:border-b-black"
+                href={dirPage.path}
+              >
+                <span class="flex-grow text-black">{text}</span>
+                <span class="">
+                  {date}
+                </span>
+              </a>
             </li>
           )
         })}
