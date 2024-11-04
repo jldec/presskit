cb95d16a98fbbdd979d53bbe6a0b6b64529eb014
jldec <jurgen@jldec.me>
2024-11-04 10:50:30 +0000

    fix icon aria labels

--- src/worker/src/components/icons.tsx
+++ src/worker/src/components/icons.tsx
@@ -44,7 +44,7 @@ export function SquareChevronUp(props: IconProps) {
       stroke-linejoin="round"
       class={props.class}
       role="img"
-      aria-label={props.name || 'GitHub'}
+      aria-label={props.name || 'Up Arrow Icon'}
     >
       <rect width="18" height="18" x="3" y="3" rx="2" />
       <path d="m8 14 4-4 4 4" />
@@ -63,7 +63,7 @@ export function SquareChevronDown(props: IconProps) {
       stroke-linejoin="round"
       class={props.class}
       role="img"
-      aria-label={props.name || 'GitHub'}
+      aria-label={props.name || 'Down Arrow Icon'}
     >
       <rect width="18" height="18" x="3" y="3" rx="2" />
       <path d="m16 10-4 4-4-4" />
@@ -82,7 +82,7 @@ export function SquareChevronLeft(props: IconProps) {
       stroke-linejoin="round"
       class={props.class}
       role="img"
-      aria-label={props.name || 'GitHub'}
+      aria-label={props.name || 'Left Arrow Icon'}
     >
       <rect width="18" height="18" x="3" y="3" rx="2" />
       <path d="m14 16-4-4 4-4" />
@@ -101,7 +101,7 @@ export function SquareChevronRight(props: IconProps) {
       stroke-linejoin="round"
       class={props.class}
       role="img"
-      aria-label={props.name || 'GitHub'}
+      aria-label={props.name || 'Right Arrow Icon'}
     >
       <rect width="18" height="18" x="3" y="3" rx="2" />
       <path d="m10 8 4 4-4 4" />
@@ -120,7 +120,7 @@ export function List(props: IconProps) {
       stroke-linejoin="round"
       class={props.class}
       role="img"
-      aria-label={props.name || 'GitHub'}
+      aria-label={props.name || 'List Icon'}
     >
       <path d="M3 12h.01" />
       <path d="M3 18h.01" />
@@ -143,7 +143,7 @@ export function Email(props: IconProps) {
       stroke-linejoin="round"
       class={props.class}
       role="img"
-      aria-label={props.name || 'Email'}
+      aria-label={props.name || 'Email Icon'}
     >
       <rect width="20" height="16" x="2" y="4" rx="2" />
       <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
@@ -162,7 +162,7 @@ export function Home(props: IconProps) {
       stroke-linejoin="round"
       class={props.class}
       role="img"
-      aria-label={props.name || 'Home'}
+      aria-label={props.name || 'Home Icon'}
     >
       <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
       <polyline points="9 22 9 12 15 12 15 22" />
@@ -182,7 +182,7 @@ export function Twitter(props: IconProps) {
       stroke-width="0"
       class={props.class}
       role="img"
-      aria-label={props.name || 'Twitter'}
+      aria-label={props.name || 'Twitter Icon'}
     >
       <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
     </svg>
@@ -196,7 +196,7 @@ export function GitHub(props: IconProps) {
       fill="currentcolor"
       class={props.class}
       role="img"
-      aria-label={props.name || 'GitHub'}
+      aria-label={props.name || 'GitHub Icon'}
     >
       <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z" />
     </svg>
@@ -210,7 +210,7 @@ export function Linkedin(props: IconProps) {
       fill="currentcolor"
       class={props.class}
       role="img"
-      aria-label={props.name || 'LinkedIn'}
+      aria-label={props.name || 'Linkedin Icon'}
     >
       <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z" />
     </svg>
