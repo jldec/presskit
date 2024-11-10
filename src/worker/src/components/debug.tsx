fb47f1cca49e21ee12e0940a0ee20ebd695bd302
jldec <jurgen@jldec.me>
2024-11-10 05:56:54 -0500

    minimal debug user

--- src/worker/src/components/debug.tsx
+++ src/worker/src/components/debug.tsx
@@ -1,9 +1,10 @@
 import { type FC } from 'hono/jsx'
 import { useRequestContext } from 'hono/jsx-renderer'
+import { type Context } from '../types'
 
 export const Debug: FC = ({ page }) => {
-  const c = useRequestContext()
-  const user = c.req.header('cf-access-authenticated-user-email')
+  const c: Context = useRequestContext()
+  const user = c.req.header('cf-access-authenticated-user-email') || c.env.DEBUG_USER || 'unauthenticated'
   if (page && c.env.DEBUG)
     return (
       <details class="mt-4 cursor-pointer">
