7f04bd678f06e955a48e551ef599bd0594f6372b
Jürgen Leschner <jldec@users.noreply.github.com>
2024-11-12 14:04:53 -0600

    use email instead of fake names (#34)

--- src/worker/src/components/chat.tsx
+++ src/worker/src/components/chat.tsx
@@ -1,14 +1,18 @@
 import { type FC } from 'hono/jsx'
 import { MessageSquare } from './icons'
+import { useRequestContext } from 'hono/jsx-renderer'
+import { type Context } from '../types'
 
 export const Chat: FC = () => {
+  const c: Context = useRequestContext()
   return (
     <>
       <nav class="flex md:mb-2">
+        <span id="chat-user" class="hidden">{c.get('user')}</span>
         <a
           class="ml-auto mr-[6px] md:mr-0"
           role="button"
-          onclick="document.getElementById('chat-root').classList.toggle('hidden')"
+          onclick="document.getElementById('chat-root').classList.toggle('hidden');document.getElementById('chat-user').classList.toggle('hidden')"
         >
           <MessageSquare class="h-5 hover:text-orange-500 transition-colors duration-200 ease-in-out" />
         </a>
